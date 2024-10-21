9ad364cd7dc4778c44dfbd445607392acbe0ec2c
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2024-10-21 22:07:22 +0700

    feat: admin lte

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,10 +2,9 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 use App\Models\Admin;
 use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PostFactory extends Factory
 {
