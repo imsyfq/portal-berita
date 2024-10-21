0a74f396983e19ad71dc28b8bc6c54bcc7f38e5f
Syafiq Afifuddin <56501562+tb12as@users.noreply.github.com>
2024-10-21 22:16:16 +0700

    Admin lte (#2)
    * feat: database structure using blueprint
    
    * feat: admin lte

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -2,9 +2,8 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 use App\Models\Admin;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class AdminFactory extends Factory
 {
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('front-end.index');
+});
+
+Route::prefix('admin')->group(function () {
+    Auth::routes([
+        'register' => false, // Registration Routes...
+        'reset' => false, // Password Reset Routes...
+        'verify' => false, // Email Verification Routes...
+    ]);
+
+    Route::middleware('auth:admin')->group(function () {
+        Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    });
 });
