47f09c17a52744baa9dfa3a8f9da728c1fda42d4
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2024-11-08 16:44:13 +0700

    fix: remove adminer middlware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\AdminerMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'adminer-access' => AdminerMiddleware::class,
+            // 'adminer-access' => \App\Http\Middleware\AdminerMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
